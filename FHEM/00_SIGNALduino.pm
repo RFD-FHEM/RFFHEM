871ff617f4de0302500d7391bb7d3d1ef211982a
Jörg <joerg.koehn@t-online.de>
2015-09-27 21:11:40 +0200

    Update 00_SIGNALduino.pm
    sub SIGNALDuino_use_longid{} eingefügt, wird im 14_Cresta.pm benötigt.

--- FHEM/00_SIGNALduino.pm
+++ FHEM/00_SIGNALduino.pm
@@ -1775,7 +1775,14 @@ sub	SIGNALduino_Cresta()
 	return (-1,"");
 }
 
-
+# Helper Function for logical Modules, to check if they should use longids
+sub SIGNALDuino_use_longid {
+  my ($iohash,$dev_type) = @_;
+  my $longids=AttrVal($iohash->{NAME},"longids","0");	      				# Default to not use longids
+  return 0 if ($longids eq "") || ($longids eq "0");
+  return 1 if ($longids eq "1") || ($longids eq "ALL") || (",$longids," =~ m/,$dev_type,/) ;
+  return 0;
+}
 
 
 
