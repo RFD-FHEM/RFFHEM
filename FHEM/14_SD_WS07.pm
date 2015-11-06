23234e8de9fc496148bf03b5220455f0001b8fb6
sidey79 <s.butzek@gmx.de>
2015-11-06 23:20:42 +0100

    Signalduino: Updated Firmware 3.2.0-b2

--- FHEM/14_SD_WS07.pm
+++ FHEM/14_SD_WS07.pm
@@ -267,6 +267,7 @@ sub SD_WS07_Attr(@)
   <ul>
     <li>Eurochon EAS800z</li>
     <li>Technoline WS6750/TX70DTH</li>
+    <li>TFA 30320902</li>
   </ul>
   <br>
   Neu empfangene Sensoren werden in FHEM per autocreate angelegt.
