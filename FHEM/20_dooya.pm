8fca923b22a71632f925155b10354f9017fb6166
Jarnsen <jarno.karsch@vodafone.de>
2016-02-18 20:28:46 +0100

    Update 20_dooya.pm

--- FHEM/20_dooya.pm
+++ FHEM/20_dooya.pm
@@ -57,7 +57,7 @@ my $dooya_defrepetition = 6;	# Default Dooya frame repeat counter
 
 my $dooya_updateFreq = 3;	# Interval for State update
 
-my %models = ( dooyablinds => 'blinds', dooyashutter => 'shutter', ); # supported models (blinds  and shutters)
+#my %models = ( dooyablinds => 'blinds', dooyashutter => 'shutter', ); # supported models (blinds  and shutters)
 my %channal = (  0 => '0000', 1 => '0001', 2 => '0010', 3 => '0011', 4 => '0100', 5 => '0101', 6 => '0110', 7 => '0111', 8 => '1000', 9 => '1001', 10 => '1010', 11 => '1011', 12 => '1100', 13 => '1101', 14 => '1110', 15 => '1111', ); #Kanal
 my %SignalRepeats = ( 5 => '#R5', 10 => '#R10', 15 => '#R15', 20 => '#R20', );  # Sendungswiederholungen des Signales
 
@@ -520,7 +520,7 @@ sub Dooya_InternalSet($@) {
   my ($t1down100, $t1downclose, $t1upopen, $t1up100) = Dooya_getTimingValues($hash);
 	#Log3($name,5,"Dooya_set: $name -> timings ->  td1:$t1down100: tdc :$t1downclose:  tuo :$t1upopen:  tu1 :$t1up100: ");
 
-	my $model =  AttrVal($name,'model',$models{dooyablinds});
+	#my $model =  AttrVal($name,'model',$models{dooyablinds});
 	
 	if($cmd eq 'pos') {
 		return "Dooya_set: No pos specification"  if(!defined($arg1));
