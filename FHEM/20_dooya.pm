db0e4e8d66615916b3e814c581461d8a7b44e25e
Jarnsen <jarno.karsch@vodafone.de>
2016-02-18 19:40:40 +0100

    Update 20_dooya.pm

--- FHEM/20_dooya.pm
+++ FHEM/20_dooya.pm
@@ -59,7 +59,7 @@ my $dooya_updateFreq = 3;	# Interval for State update
 
 my %models = ( dooyablinds => 'blinds', dooyashutter => 'shutter', ); # supported models (blinds  and shutters)
 my %channal = (  0 => '0000', 1 => '0001', 2 => '0010', 3 => '0011', 4 => '0100', 5 => '0101', 6 => '0110', 7 => '0111', 8 => '1000', 9 => '1001', 10 => '1010', 11 => '1011', 12 => '1100', 13 => '1101', 14 => '1110', 15 => '1111', ); #Kanal
-my %SignalRepeats = ( 5 => '#R5', 10 => '#R10', 15 => '#R15', 20 => '#R20', );" # Sendungswiederholungen des Signales
+my %SignalRepeats = ( 5 => '#R5', 10 => '#R10', 15 => '#R15', 20 => '#R20', );  # Sendungswiederholungen des Signales
 
 
 ######################################################
