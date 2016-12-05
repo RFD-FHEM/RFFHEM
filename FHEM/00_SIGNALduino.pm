7cb839043fb47ce484ecb0aac88524f732fa4643
sidey79 <s.butzek@gmx.de>
2016-12-05 22:31:32 +0100

    00_SIGNALduino.pm
    Fixed warning message, when deleting one attribute

--- FHEM/00_SIGNALduino.pm
+++ FHEM/00_SIGNALduino.pm
@@ -2754,7 +2754,8 @@ SIGNALduino_Attr(@)
 	my ($cmd,$name,$aName,$aVal) = @_;
 	my $hash = $defs{$name};
 	my $debug = AttrVal($name,"debug",0);
-
+	
+	$aVal= "null" if (!defined($aVal));
 	Log3 $name, 4, "$name: Calling Getting Attr sub with args: $cmd $aName = $aVal";
 		
 	if( $aName eq "Clients" ) {		## Change clientList
