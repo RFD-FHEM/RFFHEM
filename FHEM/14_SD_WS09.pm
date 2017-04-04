07c9cc89f76b9cf6768a037acc95cdb313a1f63f
Jörg <joerg.koehn@t-online.de>
2017-04-04 18:40:57 +0200

    Update 14_SD_WS09.pm
    UV/Lux ID angepaßt

--- FHEM/14_SD_WS09.pm
+++ FHEM/14_SD_WS09.pm
@@ -219,7 +219,7 @@
                   # symbol FOluxMo = 5 ; Lux Middle byte
                   # symbol FOluxLo = 6 ; Lux Low byte, Unit = 0.1 Lux (binary)
                   # symbol FOcksumo= 7 ; CRC checksum (CRC-8 shifting left)
-                  $id = SD_WS09_bin2dec(substr($sensdata,8,8));
+                  $id = SD_WS09_bin2dec(substr($sensdata,4,8));
                   $FOuvo = SD_WS09_bin2dec(substr($sensdata,12,4));
                   $FOlux = SD_WS09_bin2dec(substr($sensdata,24,24))/10;
                   Log3 $iohash, 3, "$name: SD_WS09_Parse7 UV-Solar1: id:$id, UV:".$FOuvo." Lux:".$FOlux ;
