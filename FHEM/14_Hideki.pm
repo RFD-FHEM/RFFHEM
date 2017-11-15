6489e8f2df86b5be156a07b5e96a4955993bcc25
HomeAutoUser <abfall-container@gmx.de>
2017-11-15 17:34:05 +0100

    14_Hideki.pm - Fix
    - comfort_level Länge FHEM db Modul angepasst von max 32. Zeichen
    - Ausgabe comfort_level erfolgt in Logfile als Wert für LogfileDaten reduzierung, im Reading erscheint Text
    - package_number für Sensor 0x1F geprüft

--- FHEM/14_Hideki.pm
+++ FHEM/14_Hideki.pm
@@ -126,7 +126,7 @@ Hideki_Parse($$)
 		($channel, $temp) = decodeThermo(\@decodedBytes); # decodeThermoHygro($decodedString);
 		$hum = 10 * ($decodedBytes[6] >> 4) + ($decodedBytes[6] & 0x0f);
 		$bat = ($decodedBytes[2] >> 6 == 3) ? 'ok' : 'low';			 # decode battery
-		$count = $decodedBytes[3] >> 6;
+		$count = $decodedBytes[3] >> 6;		# verifiziert, MSG_Counter
 		$comfort = ($decodedBytes[7] >> 2 & 0x03);   # comfort level
 
 		Log3 $iohash, 3, "$name: model=$model, id=$id, ch=$channel, cnt=$count, bat=$bat, temp=$temp, hum=$hum, comfort=$comfort";
@@ -139,22 +139,26 @@ Hideki_Parse($$)
 	}elsif($sensorTyp==31){
 		($channel, $temp) = decodeThermo(\@decodedBytes);
 		$bat = ($decodedBytes[2] >> 6 == 3) ? 'ok' : 'low';			 # decode battery
+		$count = $decodedBytes[3] >> 6;		# verifiziert, MSG_Counter
 		$val = "T: $temp";
 		Log3 $iohash, 4, "$name decoded Hideki protocol model=$model, sensor id=$id, channel=$channel, , cnt=$count, bat=$bat, temp=$temp";
 	}elsif($sensorTyp==14){
 		($channel, $rain) = decodeRain(\@decodedBytes); # decodeThermoHygro($decodedString);
 		$bat = ($decodedBytes[2] >> 6 == 3) ? 'ok' : 'low';			 # decode battery
+		$count = $decodedBytes[3] >> 6;		# UNVERIFIZIERT, MSG_Counter
 		$val = "R: $rain";
 		Log3 $iohash, 4, "$name decoded Hideki protocol model=$model, sensor id=$id, channel=$channel, bat=$bat, rain=$rain, unknown=$unknown";
 	}elsif($sensorTyp==12){
 		($channel, $temp) = decodeThermo(\@decodedBytes); # decodeThermoHygro($decodedString);
 		($windchill,$windspeed,$windgust,$winddir,$winddirdeg,$winddirtext) = wind(\@decodedBytes);
 		$bat = ($decodedBytes[2] >> 6 == 3) ? 'ok' : 'low';			 # decode battery
+		$count = $decodedBytes[3] >> 6;		# UNVERIFIZIERT, MSG_Counter
 		$val = "T: $temp  Ws: $windspeed  Wg: $windgust  Wd: $winddirtext";
 		Log3 $iohash, 4, "$name decoded Hideki protocol model=$model, sensor id=$id, channel=$channel, bat=$bat, temp=$temp, Wc=$windchill, Ws=$windspeed, Wg=$windgust, Wd=$winddir, WdDeg=$winddirdeg, Wdtxt=$winddirtext";
 	}elsif($sensorTyp==13){
 		($channel, $temp) = decodeThermo(\@decodedBytes); # decodeThermoHygro($decodedString);
 		$bat = ($decodedBytes[2] >> 6 == 3) ? 'ok' : 'low';			 # decode battery
+		$count = $decodedBytes[3] >> 6;		# UNVERIFIZIERT, MSG_Counter
 		$val = "T: $temp";
 		Log3 $iohash, 4, "$name decoded Hideki protocol model=$model, sensor id=$id, channel=$channel, bat=$bat, temp=$temp";
 		Log3 $iohash, 4, "$name Sensor Typ $sensorTyp currently not full supported, please report sensor information!";
