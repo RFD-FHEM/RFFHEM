4f949a3ef7852dd8184166b2c1e97455d73ec295
sidey79 <7968127+sidey79@users.noreply.github.com>
2017-11-22 00:31:31 +0100

    Max length (Proto 51) is to small (#79929 #msg719499) Raised to 45 because this is the predictive caluclation result
    (cherry picked from commit f48a7a56c3462418ffbd80d661b9db835e293bc0)

--- FHEM/00_SIGNALduino.pm
+++ FHEM/00_SIGNALduino.pm
@@ -1056,7 +1056,7 @@ my %ProtocolListSIGNALduino  = (
 			length_max      => '48',
 		},
 	 "51"    => 
-        {
+        {  # MS;P0=-16046;P1=552;P2=-1039;P3=983;P5=-7907;P6=-1841;P7=-4129;D=15161716171616161717171716161616161617161717171717171617171617161716161616161616171032323232;CP=1;SP=5;O;
             name			=> 'weather51',		# Logilink, NC, WS, TCM97001 etc.
 			comment			=> 'IAN 275901 Wetterstation Lidl',
 			id          	=> '51',
@@ -1070,7 +1070,7 @@ my %ProtocolListSIGNALduino  = (
 			clientmodule    => 'SD_WS',   
 			modulematch     => '^W51#.*',
 			length_min      => '40',
-			length_max      => '40',
+			length_max      => '45',
         },
     "52"    => 			## Oregon PIR Protocol
 		{
