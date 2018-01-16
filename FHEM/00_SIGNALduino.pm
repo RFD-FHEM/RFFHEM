c49baa1216defdaf92906a0830ab01795044554b
HomeAutoUser <abfall-container@gmx.de>
2018-01-16 16:28:35 +0100

    00_SIGNALduino.pm
    - FS20 | FHT80 PearlWarning FIX
    - Pearlwarning Option added, helpful when needed
    (https://de.perlmaven.com/use-of-uninitialized-value)
    
    (cherry picked from commit 58591531cb94f2caadb0c9e7aa39b992bc73d1f5)

--- FHEM/00_SIGNALduino.pm
+++ FHEM/00_SIGNALduino.pm
@@ -19,6 +19,8 @@ use Data::Dumper qw(Dumper);
 use Scalar::Util qw(looks_like_number);
 no warnings 'portable';
 
+#$| = 1;		#Puffern abschalten, Hilfreich für PEARL WARNINGS Search
+
 #use POSIX qw( floor);  # can be removed
 #use Math::Round qw();
 
@@ -4347,9 +4349,6 @@ sub SIGNALduino_postDemo_FHT80($@) {
       if (($sum & 0xFF) == $checksum) {								## FHT80 Raumthermostat
          for($b = 0; $b < 54; $b += 9) {	                              # check parity over 6 byte
             my $parity = 0;					                              # Parity even
-            for($i = $b; $i < $b + 9; $i++) {			                  # Parity over 1 byte + 1 bit
-               $parity += $bit_msg[$i];
-            }
             if ($parity % 2 != 0) {
                SIGNALduino_Log3 $name, 3, "$name: FHT80 ERROR - Parity not even";
                return 0, undef;
