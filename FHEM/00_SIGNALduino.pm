ce89013da59ad79918641ccaf2acef1dc37a2e41
Ralf9 <ralfk_forum@gmx.de>
2018-03-12 00:10:54 +0100

    00_SIGNALduino.pm -  Id 74 (FS20): convert $dmsg to lowercase

--- FHEM/00_SIGNALduino.pm
+++ FHEM/00_SIGNALduino.pm
@@ -3206,6 +3206,7 @@ sub SIGNALduno_Dispatch($$$$$)
 		else {
 			$rssi = "";
 		}
+		$dmsg = lc($dmsg) if ($id eq '74');
 		SIGNALduino_Log3 $name, SDUINO_DISPATCH_VERBOSE, "$name Dispatch: $dmsg, $rssi dispatch";
 		Dispatch($hash, $dmsg, \%addvals);  ## Dispatch to other Modules 
 		
