e24f884636ea479dc9aca3664d8b97485f7c5c54
dwuehler <37272566+dwuehler@users.noreply.github.com>
2018-03-14 18:31:15 +0100

    Update 14_SD_WS_Maverick.pm
    code dokumentation; fixed typos in commandref

--- FHEM/14_SD_WS_Maverick.pm
+++ FHEM/14_SD_WS_Maverick.pm
@@ -4,6 +4,11 @@
 # The purpose of this module is to support Maverick sensors
 # Sidey79 & Cruizer 2016
 # Ralf9 2018
+#
+# CHANGED
+##############################################################################
+# Version 1.1
+#  - feature: 14_SD_WS_Maverick: rename Readings, fit to Maverick_RFXTRX now
 
 package main;
 
@@ -30,7 +35,7 @@ SD_WS_Maverick_Initialize($)
   $hash->{DefFn}     = "SD_WS_Maverick_Define";
   $hash->{UndefFn}   = "SD_WS_Maverick_Undef";
   $hash->{ParseFn}   = "SD_WS_Maverick_Parse";
-  $hash->{AttrFn}	 = "SD_WS_Maverick_Attr";
+  $hash->{AttrFn}	   = "SD_WS_Maverick_Attr";
   $hash->{AttrList}  = "IODev do_not_notify:1,0 ignore:0,1 showtime:1,0 inaktivityInterval " .
                         "$readingFnAttributes ";
   $hash->{AutoCreate} =
@@ -118,8 +123,8 @@ SD_WS_Maverick_Parse($$)
   Log3 $iohash, 4, "$name $model decoded protocolid: 47 sensor startup=$startup, temp-f=$temp_str1, temp-b=$temp_str2, checksum-s=$checksum_str";
   
   if ($startup ne '59' && $startup ne '6A') {
-      Log3 $iohash, 4, "$name $model ERROR: wrong startup=$startup (must be 59 or 6A)";
-      return '';
+    Log3 $iohash, 4, "$name $model ERROR: wrong startup=$startup (must be 59 or 6A)";
+    return '';
   }
   
   # Calculate temp from data
@@ -130,33 +135,33 @@ SD_WS_Maverick_Parse($$)
   if ($temp_str1 ne '55555') { # wenn 55555 könnte man auch disconnected setzen
     $temp_str1 =~ tr/569A/0123/;
     for ( my $i = 0; $i < length($temp_str1); $i++ ) { 
-        $c = substr( $temp_str1, $i, 1);
-        $temp_food += $c*4**(4-$i);
+      $c = substr( $temp_str1, $i, 1);
+      $temp_food += $c*4**(4-$i);
     }
     if ($temp_food <= 0 || $temp_food > 300) {
-        Log3 $iohash, 4, "$name $model ERROR: wrong temp-food=$temp_food";
-        $temp_food = "";
+      Log3 $iohash, 4, "$name $model ERROR: wrong temp-food=$temp_food";
+      $temp_food = "";
     }
   } else {
-      $temp_food = "";
+    $temp_food = "";
   }
     
   if ($temp_str2 ne '55555') { # wenn 55555 könnte man auch disconnected setzen
     $temp_str2 =~ tr/569A/0123/;
     for ( my $i = 0; $i < length($temp_str2); $i++ ) { 
-        $c = substr( $temp_str2, $i, 1);
-        $temp_bbq += $c*4**(4-$i);
+      $c = substr( $temp_str2, $i, 1);
+      $temp_bbq += $c*4**(4-$i);
     }
     if ($temp_bbq <= 0 || $temp_bbq > 300) {
-        Log3 $iohash, 4, "$name $model ERROR: wrong temp-bbq=$temp_bbq";
-        $temp_bbq = "";
+      Log3 $iohash, 4, "$name $model ERROR: wrong temp-bbq=$temp_bbq";
+      $temp_bbq = "";
     }
   } else {
-      $temp_bbq = "";
+    $temp_bbq = "";
   }
   
   if ($temp_bbq eq "" && $temp_food eq "") {
-       return '';
+    return '';
   }
   
   Log3 $iohash, 4, "$name $model decoded protocolid: temp-food=$temp_food, temp-bbq=$temp_bbq;";
@@ -167,50 +172,54 @@ SD_WS_Maverick_Parse($$)
   $def = $modules{SD_WS_Maverick}{defptr}{$model} if(!$def);
 
   if(!$def) {
-      Log3 $iohash, 1, "$name SD_WS_Maverick: UNDEFINED sensor $model";
-      return "UNDEFINED $model SD_WS_Maverick $model";
+    Log3 $iohash, 1, "$name SD_WS_Maverick: UNDEFINED sensor $model";
+    return "UNDEFINED $model SD_WS_Maverick $model";
   }
-      #Log3 $iohash, 3, 'SD_WS_Maverick: ' . $def->{NAME} . ' ' . $id;
+  #Log3 $iohash, 3, 'SD_WS_Maverick: ' . $def->{NAME} . ' ' . $id;
 
-    my $hash = $def;
-    $name = $hash->{NAME};
-    Log3 $name, 4, "SD_WS_Maverick: $name ($rawData)";  
+  my $hash = $def;
+  $name = $hash->{NAME};
+  Log3 $name, 4, "SD_WS_Maverick: $name ($rawData)";  
 
-    if (!defined(AttrVal($hash->{NAME},"event-min-interval",undef)))
-    {
-        my $minsecs = AttrVal($iohash->{NAME},'minsecs',0);
-        if($hash->{lastReceive} && (time() - $hash->{lastReceive} < $minsecs)) {
-            Log3 $hash, 4, "$model Dropped due to short time. minsecs=$minsecs";
-            return "";
-        }
+  if (!defined(AttrVal($hash->{NAME},"event-min-interval",undef)))
+  {
+    my $minsecs = AttrVal($iohash->{NAME},'minsecs',0);
+    if($hash->{lastReceive} && (time() - $hash->{lastReceive} < $minsecs)) {
+      Log3 $hash, 4, "$model Dropped due to short time. minsecs=$minsecs";
+      return "";
     }
+  }
 
-    $hash->{lastReceive} = time();
-    $hash->{lastMSG} = $rawData;
-    #$hash->{bitMSG} = $bitData2; 
-    my $inaktivityInterval=int(AttrVal($name,"inaktivityInterval",5));
-    if ($temp_bbq ne "") {
-        RemoveInternalTimer($hash, 'SD_WS_Maverick_ClearTempBBQ');
-        InternalTimer(time()+($inaktivityInterval*60), 'SD_WS_Maverick_ClearTempBBQ', $hash, 0);
-    }
-    if ($temp_food ne "") {
-        RemoveInternalTimer($hash, 'SD_WS_Maverick_ClearTempFood');
-        InternalTimer(time()+($inaktivityInterval*60), 'SD_WS_Maverick_ClearTempFood', $hash, 0);
-    }
-    $checksum_str =~ tr/569A/0123/;
-    my $checksum="";
-    #for ( my $i = 0; $i < length($checksum_str); $i++ ) { 
-    #    $c = substr( $checksum_str, $i, 1);
-    #    $checksum += $c*4**(4-$i);
-    #}
-    $checksum=$checksum_str;
-    $hash->{checksum}=$checksum;
-    
+  $hash->{lastReceive} = time();
+  $hash->{lastMSG} = $rawData;
+  #$hash->{bitMSG} = $bitData2; 
   
-    Log3 $iohash, 5, "$name $model statistic: checksum=$checksum, t1=$temp_str1, temp-food=$temp_food, t2_$temp_str2, temp-bbq=$temp_bbq;";
-    SD_WS_Maverick_updateReadings($hash, $temp_food, $temp_bbq, $startup);
+  # Die Temperaturen bei Inaktivität zurücksetzen lassen durch Timer ...
+  my $inaktivityInterval=int(AttrVal($name,"inaktivityInterval",6));
+  if ($temp_bbq ne "") {
+      RemoveInternalTimer($hash, 'SD_WS_Maverick_ClearTempBBQ');
+      InternalTimer(time()+($inaktivityInterval*60), 'SD_WS_Maverick_ClearTempBBQ', $hash, 0);
+  }
+  if ($temp_food ne "") {
+      RemoveInternalTimer($hash, 'SD_WS_Maverick_ClearTempFood');
+      InternalTimer(time()+($inaktivityInterval*60), 'SD_WS_Maverick_ClearTempFood', $hash, 0);
+  }
+  
+  # Checksum auswerten
+  $checksum_str =~ tr/569A/0123/;
+  my $checksum="";
+  $checksum=$checksum_str; 
+  # TODO: Die eigentliche Checksum errechnen. Diese ändert sich bei jedem Temperaturwechsel
+  # TODO: Evtl. ist in den checksum-bits auch noch eine Info zur Batterie enthalten
+  #       ggf. ist es möglich die checksum als ID zu verwenden und so mehrere Mavericks in fhem einbinden zu können.
+  $hash->{checksum}=$checksum;
+  
+  # TODO: Logging kann entfernt werden, wenn checksum entschlüsselt ist. Wird zur Analyse verwendet.
+  Log3 $hash, 4, "$name statistic: checksum=$checksum, t1=$temp_str1, temp-food=$temp_food, t2_$temp_str2, temp-bbq=$temp_bbq;";
+  
+  SD_WS_Maverick_updateReadings($hash, $temp_food, $temp_bbq, $startup);
 
-    return $name;
+  return $name;
 
 }
 
@@ -240,7 +249,7 @@ sub SD_WS_Maverick_Attr(@)
 sub SD_WS_Maverick_ClearTempBBQ($){
   my ($hash) = @_;
   my $name = $hash->{NAME};
-  Log3 $hash, 4, "$name ClearTempBBQ";
+  Log3 $hash, 5, "$name ClearTempBBQ";
   
   my $temp_bbq=-1;
   my $temp_food=ReadingsVal($name,"temp-food",-1);
@@ -250,7 +259,7 @@ sub SD_WS_Maverick_ClearTempBBQ($){
 sub SD_WS_Maverick_ClearTempFood($){
   my ($hash) = @_;
   my $name = $hash->{NAME};
-  Log3 $hash, 4, "$name ClearTempFood";
+  Log3 $hash, 5, "$name ClearTempFood";
 
   my $temp_bbq=ReadingsVal($name,"temp-bbq",-1);  
   my $temp_food=-1;
@@ -289,7 +298,7 @@ sub SD_WS_Maverick_updateReadings($$$$){
 =begin html
 
 <a name="SD_WS_Maverick"></a>
-<h3>Wether Sensors protocol #7</h3>
+<h3>Weather Sensors protocol #47</h3>
 <ul>
   The SD_WS_Maverick module interprets temperature sensor messages received by a Device like CUL, CUN, SIGNALduino etc.<br>
   <br>
@@ -311,7 +320,7 @@ sub SD_WS_Maverick_updateReadings($$$$){
   <a name="SD_WS_Maverick Events"></a>
   <b>Generated readings:</b>
   <ul>
-  	 <li>State (BBQ: Food: )</li>
+  	 <li>State (Food: BBQ: )</li>
      <li>temp-bbq (&deg;C)</li>
      <li>temp-food (&deg;C)</li>
      <li>messageType (6A at startup or rsync, otherwise 59)</li>
@@ -322,7 +331,7 @@ sub SD_WS_Maverick_updateReadings($$$$){
   <ul>
     <li>inaktivityInterval <minutes (1-60)><br>
     The Interval to set temp-bbq and/or temp-food to -1 after defined minutes. Empty batteries or the malfunction of a tempertature-sensor could be recognized.<br> 
-    <code>default: 5</code></li>
+    <code>default: 6</code></li>
     <li><a href="#do_not_notify">do_not_notify</a></li>
     <li><a href="#ignore">ignore </a></li>
     <li><a href="#showtime">showtime (see FHEMWEB)</a></li>
@@ -357,14 +366,14 @@ sub SD_WS_Maverick_updateReadings($$$$){
   <a name="SD_WS_Maverick_Define"></a>
   <b>Define</b> 
   <ul>Die empfangenen Sensoren werden automatisch angelegt.<br>
-  Da das Maverick bei jedem Start eine neue zufällige ID erzeugt kann das Gerät nicht mit dem fhem-device gekoppelt werden. 
-  Das bedeutet, dass es nicht möglich ist in fhem zwei Mavericks parallel zu betreiben.
+  Da das Maverick bei jedem Start eine neue zufällige ID erzeugt kann das Ger&aumlt nicht mit dem fhem-device gekoppelt werden. 
+  Das bedeutet, dass es nicht m&oumlglich ist in fhem zwei Mavericks parallel zu betreiben.
   </ul>
   <br>
   <a name="SD_WS_Maverick Events"></a>
   <b>Generierte Readings:</b>
   <ul>
-  	 <li>State (BBQ: Food: )</li>
+  	 <li>State (Food: BBQ: )</li>
      <li>temp-bbq (&deg;C)</li>
      <li>temp-food (&deg;C)</li>
      <li>messageType (6A bei Start oder rsync, sonst 59)</li>
@@ -375,8 +384,8 @@ sub SD_WS_Maverick_updateReadings($$$$){
   <ul>
     <li>inaktivityInterval <minutes (1-60)><br>
     Das Interval nach dem temp-bbq und/oder temp-food auf -1 gesetzt werden, wenn keine Signale mehr empfangen werden.
-    Hilfreich zum erkennen einer leeren Batterie oder eines defekten Termperaturfühlers.<br> 
-    <code>default: 5</code></li>
+    Hilfreich zum erkennen einer leeren Batterie oder eines defekten Termperaturf&uumlhlers.<br> 
+    <code>default: 6</code></li>
     <li><a href="#do_not_notify">do_not_notify</a></li>
     <li><a href="#ignore">ignore</a></li>
     <li><a href="#showtime">showtime</a></li>
