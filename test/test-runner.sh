6235f3298c436815c8ab1c9c1156f9ded9d2f115
sidey79 <7968127+sidey79@users.noreply.github.com>
2018-09-23 01:01:28 +0200

    test-runner.sh
    Improved runner to get proper return code, output and detection of test result

--- test/test-runner.sh
+++ test/test-runner.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Return 0 when test run is okay
+# Return 1 when there was an test error
+
 
 #FHEM_SCRIPT = "/opt/fhem/fhem.pl"
 FHEM_SCRIPT='./fhem.pl'
-
+IFS=
 # Start the fhem instance with a test config file
-perl $FHEM_SCRIPT fhemtest.cfg
+#perl $FHEM_SCRIPT fhemtest.cfg
+RETURN=$(perl $FHEM_SCRIPT 7072 "reload 98_UnitTest")
+echo $RETURN
+echo "--------- Starting test $1 ---------\n\n"
 
-# Add Test definitions online and prepare them for import
+# Load test definitions, and import them to our running instance
 
 CMD=$( sed '/{/,/}/s/;/;;/g' ../test/"$1"-definition.txt)
 #echo $CMD
@@ -17,20 +23,58 @@ RETURN=$(perl $FHEM_SCRIPT 7072 "$CMD")
 
 echo $RETURN
 
-#Wait until test is finished
+#Wait until state of current test is finished
+#Todo prevent forever loop here
 CMD="{ReadingsVal(\"$1\",\"state\",\"\")}"
 until [ $(perl $FHEM_SCRIPT 7072 "$CMD") == "finished" ] ; do 
   sleep 1; 
 done
 
-CMD='"{ReadingsVal(\"$1\",\"test_output\",\"\")}"
+CMD="{ReadingsVal(\"$1\",\"test_output\",\"\")}"
 OUTPUT=$(perl $FHEM_SCRIPT 7072 "$CMD")
+OUTPUT=$(echo $OUTPUT | awk '{gsub(/\\n/,"\n")}1')
 
-CMD='"{ReadingsVal(\"$1\",\"test_failure\",\"\")}"
+CMD="{ReadingsVal(\"$1\",\"test_failure\",\"\")}"
 OUTPUT_FAILED=$(perl $FHEM_SCRIPT 7072 "$CMD")
 
-perl $FHEM_SCRIPT 7072 "shutdown"
+	#echo $OUTPUT
+	#echo $OUTPUT_FAILED
 
+testlog=$(awk '/Test '"$1"' starts here ---->/,/<---- Test '"$1"' ends here/' log/fhem-*.log)
+#oklines=$(echo $testlog | egrep ^[[:digit:]]{4}\.[[:digit:]]{2}\.[[:digit:]]{2}[[:space:]][[:digit:]]{2}:[[:digit:]]{2}:[[:digit:]]{2}[[:space:]]3:[[:space:]]ok)
+#noklines=$(echo $testlog | egrep ^[[:digit:]]{4}\.[[:digit:]]{2}\.[[:digit:]]{2}[[:space:]][[:digit:]]{2}:[[:digit:]]{2}:[[:digit:]]{2}[[:space:]]3:[[:space:]]nok)
+
+#echo $testlog
+
+echo "Output of $1:\n\n"
 echo $OUTPUT
 
+if [ -z "$OUTPUT_FAILED" ]
+then
+    if [ $(echo $testlog | grep -Fxc "PERL WARNING") -gt 0 ]
+	then
+		echo "Warnings in FHEM Log snippet from test run:"
+		echo $testlog
+		status="ok with warnings"
+	else 
+		status="ok"
+	fi
+
+else
+	echo "Errors of test $1:"
+	echo $OUTPUT_FAILED
+
+	echo "FHEM Log snippet from test run:"
+	echo $testlog
+	status="error"
+fi
+
+printf "\n\n--------- Test %s: %s ---------\n" $1 $status
+
+if [ $status == "error" ] 
+then
+ exit 1
+fi
 exit 0
+
+#perl $FHEM_SCRIPT 7072 "shutdown"
