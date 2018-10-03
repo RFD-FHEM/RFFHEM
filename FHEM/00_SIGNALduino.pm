b72619a2e50615fde357b2ded1eb74e677b4fbd4
sidey79 <7968127+sidey79@users.noreply.github.com>
2018-10-04 00:07:30 +0200

    [WIP] Dev r33 unittest developid (#289)
    * 00_SIGNALduino.pm
    
    sub SIGNALduino_getProtocolList created to retrieve a reference to the protocolHash 
    
    * test/test_developid_1-definition.txt
       checks developid and correct skip of protocols. covers developId = m | y | p set and unset.
    
    * test-runner.sh
    
    fixed a bug, that caused lines with "," at end not to work
    
    * Updated Makefile

--- FHEM/00_SIGNALduino.pm
+++ FHEM/00_SIGNALduino.pm
@@ -4054,7 +4054,7 @@ sub SIGNALduino_compPattern($$$%)
 			$buckets{$key}=$patternListRaw{$key};
 		}
 	}
-
+-
 	return ($cnt,$rawData, %patternListRaw);
 	#print "rdata: ".$msg_parts{rawData}."\n";
 
@@ -4081,6 +4081,13 @@ sub SIGNALduino_Log3($$$)
   }
   
   return Log3($name,$loglevel,$text);
+}
+
+################################################
+# Helper to get a reference of the protocolList Hash
+sub SIGNALduino_getProtocolList()
+{
+	return \%ProtocolListSIGNALduino
 }
 
 #print Dumper (%msg_parts);
