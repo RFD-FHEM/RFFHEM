1f9854ce4ecf154f610c453c85d45c72f9bddde1
HomeAutoUser <abfall-container@gmx.de>
2018-10-29 14:21:18 +0100

    Update FHEM/14_SD_BELL.pm
    - regex fix (misstakes from me)

--- FHEM/14_SD_BELL.pm
+++ FHEM/14_SD_BELL.pm
@@ -61,7 +61,7 @@ sub SD_BELL_Define($$) {
 
 	# Argument					   0	 1		2		3				4
 	return "wrong syntax: define <name> SD_BELL <Protocol> <HEX-Value> <optional IODEV>" if(int(@a) < 3 || int(@a) > 5);
-	return "wrong <protocol> $a[2]" if not($a[2] =~ /^\b14\b|^\b15\b|^\b32\b|^\b41\b|^\b57\b|^\b79\b/s);
+	return "wrong <protocol> $a[2]" if not($a[2] =~ /^(?:14|15|32|41|57|79)/s);
 	### checks ###
 	return "wrong HEX-Value! Protocol $a[2] HEX-Value to short | long or not HEX (0-9 | a-f | A-F){3,8}" if (not $a[3] =~ /^[0-9a-fA-F]{3,8}/s);
 
