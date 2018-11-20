8b31c80c618061ae04e2a8d214aea078fa61448d
Udo <info@elektron-bbs.de>
2018-11-20 20:52:19 +0100

    Correct output temperature of protocol 44
    leading zeros away  (Bresser Temeo Trend)

--- FHEM/14_SD_WS.pm
+++ FHEM/14_SD_WS.pm
@@ -440,14 +440,12 @@ sub SD_WS_Parse($$)
 		my $checkTemp1 = SD_WS_binaryToNumber($binvalue, 53, 55) ^ 0b111;
 		my $checkTemp2 = SD_WS_binaryToNumber($binvalue, 56, 59) ^ 0b1111;
 		my $checkTemp3 = SD_WS_binaryToNumber($binvalue, 60, 63) ^ 0b1111;
-		$temp = $temp1Dec.$temp2Dec.".".$temp3Dec;
-		
 		if ($checkTemp1 != $temp1Dec || $checkTemp2 != $temp2Dec || $checkTemp3 != $temp3Dec)
 		{
 			Log3 $iohash, 4, "$name: SD_WS_Parse BresserTemeo checksum error in Temperature";
 			$checksumOkay = 0;
 		}
-
+		$temp = sprintf("%.1f", $temp1Dec.$temp2Dec.".".$temp3Dec);
 		if ($temp > 60)
 		{
 			Log3 $iohash, 4, "$name: SD_WS_Parse BresserTemeo Temperature Error. temp=$temp";
