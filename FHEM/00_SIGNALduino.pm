42c7131361c4490331c6e3af4a93766007a72b71
sidey79 <7968127+sidey79@users.noreply.github.com>
2019-02-23 21:28:08 +0100

    00_SIGNALduino.pm
    Moved Protocol hash into a package and load it
    
    00_SIGNALduino.pm
    
    dereference protocolHash in the sub SIGNALduino_LoadProtocolHash
    
    Makefile
    
    add copy of *.pm files in lib

--- FHEM/00_SIGNALduino.pm
+++ FHEM/00_SIGNALduino.pm
@@ -252,7 +252,8 @@ SIGNALduino_Initialize($)
   
   #ours %attr{};
 
-  %ProtocolListSIGNALduino = SIGNALduino_LoadProtocolHash("$attr{global}{modpath}/FHEM/lib/signalduino_protocols.hash");
+  %ProtocolListSIGNALduino = SIGNALduino_LoadProtocolHash("$attr{global}{modpath}/FHEM/lib/signalduino_protocols.pm");
+  #Log3 "SIGNALduino", 1, Dumper(%ProtocolListSIGNALduino);
   if (exists($ProtocolListSIGNALduino{error})  ) {
   	Log3 "SIGNALduino", 1, "Error loading Protocol Hash. Module is in inoperable mode error message:($ProtocolListSIGNALduino{error})";
   	delete($ProtocolListSIGNALduino{error});
@@ -283,17 +284,8 @@ sub SIGNALduino_LoadProtocolHash($)
 		return %{ {"error" => "File does not exsits"}};
 	}
 	
-	my $protocol_data = do {
-	open my $fh, '<', $_[0] ;
-		local $/; # Undefine $/ for this scope...
-   		<$fh>;    # so <> slurps up the entire file
-	};
-	
-	my %evalret= eval $protocol_data ;
-	if (!%evalret) {
-		return %{ {"error" => $@}};
-	} 
-	return %evalret;
+	require $_[0];
+	return %{SD_Protocols->getProtocolList};
 }
 
 
