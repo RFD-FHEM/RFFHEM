9350bd1811e4c5b4e02af4185ad345390c92c6f7
HomeAutoUser <abfall-container@gmx.de>
2020-04-21 18:45:33 +0200

    Update SD_Protocols.pm
    * revised comment

--- FHEM/lib/SD_Protocols.pm
+++ FHEM/lib/SD_Protocols.pm
@@ -197,6 +197,11 @@ sub MCRAW {
 ###       all functions for RAWmsg processing or module preparation       ###
 #############################################################################
 
+# to simple transfer
+# SIGNALduino_HE800
+# SIGNALduino_HE_EU
+# SIGNALduino_postDemo_EM
+
 ############################################################
 
 =item ConvPCA301()
@@ -212,7 +217,7 @@ Output:
 
 =cut
 
-############################################################
+
 sub ConvPCA301 {
 	my $hexData = shift // croak 'Error: called without $hexdata as input';
 	
@@ -254,7 +259,6 @@ Output:
 
 =cut
 
-############################################################
 sub ConvKoppFreeControl {
 	my $hexData = shift // croak 'Error: called without $hexdata as input';
 
