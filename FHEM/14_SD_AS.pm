8b33a559db175fb353d6180fa079006a6eda3adf
Udo <info@elektron-bbs.de>
2020-10-01 20:43:58 +0200

    feature update 14_SD_AS
    new sensor type "raw"
    new CRC8 check (optional)
    Adjustment of log outputs
    new reading "batteryState"
    new reading "batteryVoltage"

--- FHEM/14_SD_AS.pm
+++ FHEM/14_SD_AS.pm
@@ -1,12 +1,12 @@
 ##############################################
-# $Id: 14_SD_AS.pm 3.4.3 2016-24-01 00:16:11
+# $Id: 14_SD_AS.pm 350 2020-10-01 20:16:11Z elektron-bbs $
 # The file is part of the SIGNALduino project
 # see http://www.fhemwiki.de/wiki/SIGNALduino
 # and was created to provide support for self build sensors.
 # The purpos is to use it as addition to the SIGNALduino
 # modules in combination with RFDuino
 # N. Butzek, S. Butzek, 2014 
-#
+# elektron-bbs, 2020
 
 package main;
 #use version 0.77; our $VERSION = version->declare('v3.4.3');
@@ -24,26 +24,26 @@ SD_AS_Initialize($)
   #FHEM Schnittstelle:
   #    ASTTIIDDDDCC
   # AS  - Arduino Sensor
-  # TT  - Bit 0..6 type
-  #	- Bit 7 trigger (0 auto, 1 manual)
-  # II  - Bit 0..5 id
-  #	- Bit 6,7 battery status (00 - bad, 01 - change, 10 - ok, 11 - optimal)
+  # TT  - Bit 0..6 type - Bit 7 trigger (0 auto, 1 manual)
+  # II  - Bit 0..5 id   - Bit 6,7 battery status (00 - bad, 01 - change, 10 - ok, 11 - optimal)
   # DD1 - LowByte
   # DD2 - HighByte
   # CC  -  Dallas (now Maxim) iButton 8-bit CRC calculation
   #-----------------------------
-  # T	Type:
-  # 0
-  # 1
-  # 2
-  # 3   light hirange (outdoor)
-  # 4	light hires (indoor)
-  # 5	water
-  # 6	temp
-  # 7	reed gas
-  # 8    voltage
-  # 9   Humidity
+  #  T Type:
+  #  0
+  #  1 moisture
+  #  2 door
+  #  3 light hirange (outdoor)
+  #  4 light hires (indoor)
+  #  5 water
+  #  6 temperature
+  #  7 reed gas
+  #  8 voltage
+  #  9 humidity
+  # 10 raw
   # ..31 
+
   $hash->{Match}     = "^P2#[A-Fa-f0-9]{7,8}";
   $hash->{DefFn}     = "SD_AS_Define";
   $hash->{UndefFn}   = "SD_AS_Undef";
@@ -51,13 +51,14 @@ SD_AS_Initialize($)
   $hash->{AttrList}  = "do_not_notify:0,1 showtime:0,1 ignore:0,1 ".$readingFnAttributes;
   $hash->{AutoCreate}=
         { 
-          "ArduinoSensor_temp.*" => { ATTR => "event-min-interval:.*:300 event-on-change-reading:.*", FILTER => "%NAME", GPLOT => "temp4:Temp,", autocreateThreshold => "2:600"},
-          "ArduinoSensor_humidity.*" => { ATTR => "event-min-interval:.*:300 event-on-change-reading:.*", FILTER => "%NAME", GPLOT => "temp4hum4:Temp/Hum,", autocreateThreshold => "2:600"},
-          "ArduinoSensor_reedGas.*" => { ATTR => "event-min-interval:.*:1 event-on-change-reading:.*", FILTER => "%NAME", autocreateThreshold => "2:600"},
-          "ArduinoSensor_voltage.*" => { ATTR => "event-min-interval:.*:300 event-on-change-reading:.*", FILTER => "%NAME", autocreateThreshold => "2:600"},
+          "ArduinoSensor_temp.*" => { ATTR => "event-min-interval:.*:300 event-on-change-reading:.*", FILTER => "%NAME", GPLOT => "temp4:Temp,", autocreateThreshold => "3:600"},
+          "ArduinoSensor_humidity.*" => { ATTR => "event-min-interval:.*:300 event-on-change-reading:.*", FILTER => "%NAME", GPLOT => "temp4hum4:Temp/Hum,", autocreateThreshold => "3:600"},
+          "ArduinoSensor_reedGas.*" => { ATTR => "event-min-interval:.*:300 event-on-change-reading:.*", FILTER => "%NAME", autocreateThreshold => "3:600"},
+          "ArduinoSensor_moisture.*" => { ATTR => "event-min-interval:.*:300 event-on-change-reading:.*", FILTER => "%NAME", autocreateThreshold => "3:600"},
+          "ArduinoSensor_voltage.*" => { ATTR => "event-min-interval:.*:300 event-on-change-reading:.*", FILTER => "%NAME", autocreateThreshold => "3:600"},
+          "ArduinoSensor_raw.*" => { ATTR => "event-min-interval:.*:300 event-on-change-reading:.*", FILTER => "%NAME", autocreateThreshold => "3:600"},
         };
 }
-
 
 #####################################
 sub
@@ -92,117 +93,128 @@ SD_AS_Undef($$)
 sub
 SD_AS_Parse($$)
 {
-  	my ($iohash,$msg) = @_;
- 	my (undef ,$rawData) = split("#",$msg);
+	my ($iohash,$msg) = @_;
+	my (undef ,$rawData) = split("#",$msg);
 	my $name = $iohash->{NAME};
 
-	
   if (length($rawData) < 8) {
-    Log3 $iohash, 4, "SD_AS: wrong message -> $rawData";
-    return "";
+    Log3 $iohash, 3, "$name: SD_AS ERROR - message $rawData too short";
+    return q{};
   }
-  ### CRC Check only if more than 10 characters for backward compatibility ###
+  ### CRC Check only if more than 8 characters for backward compatibility ###
   if (length($rawData) > 8) {
-	my $i;
-	my $crc = 0x0;
-	my $byte;
-	
-    for ($i = 2; $i <= 8; $i+=2) {
-      #convert pairs of hex digits into number for Bytes 0-3 only!
-      $byte = hex(substr $rawData, $i, 2);
-      $crc=SD_AS_crc($crc,$byte);
+		my $rc = eval	{
+			require Digest::CRC;
+			Digest::CRC->import();
+			1;
+		};
+		if ($rc) {
+			my $ctx = Digest::CRC->new( width => 8, poly => 0x31 );
+			my $calcCrc = $ctx->add( pack 'H*', substr( $rawData, 0, 8 ) )->digest;
+			my $checksum = sprintf( "%d", hex( substr( $rawData, 8, 2 ) ) );
+			if ($calcCrc != $checksum) {
+				Log3 $iohash, 3, "$name: SD_AS ERROR - Calculated CRC ($calcCrc) not match received ($checksum) in msg $rawData";
+				return q{};
+			}
+		} else {
+			Log3 $name, 1, "$name: SD_AS ERROR - CRC not load, please install modul Digest::CRC";
+			# return q{};
+		}
 	}
-	my $crc8;
-	$crc8 = substr($rawData,-2);  ## Get last two Ascii Chars for CRC Validation
 
-	if ($crc != hex($crc8)) {
-	  Log3 $iohash, 4, "AS: CRC ($crc) does not not match $rawData, should be CRC ($crc8)";
-
-	  return undef;
-	  
+	if ((hex(substr($rawData,0,2)) & 0x7f) > 10) {
+		Log3 $iohash, 3, "$name: SD_AS ERROR - ArduinoSensor unknown model " . (hex(substr($rawData,0,2)) & 0x7f);
+		return q{};
 	}
-  }
 
+  Log3 $iohash, 4, "$name: SD_AS $rawData";
 
-  Log3 $iohash, 4, "AS: $rawData";
-  
-  my ($deviceCode, $SensorTyp, $model, $id, $valHigh, $valLow, $Sigval, $bat, $trigger, $val, $sigType);
-  # T	Type:
-  # 0
-  # 1	moisture
-  # 2	door
-  # 3   light hirange (outdoor)
-  # 4	light hires (indoor)
-  # 5	water
-  # 6	temp
-  # 7	reed gas
-  # 8    voltage
-  # 9   Humidity
+  my ($deviceCode, $SensorTyp, $model, $id, $valHigh, $valLow, $Sigval, $bat, $batteryVoltage, $batteryState, $batteryPercent, $trigger, $val, $sigType);
+  #  T Type:
+  #  0
+  #  1	moisture
+  #  2	door
+  #  3 light hirange (outdoor)
+  #  4	light hires (indoor)
+  #  5	water
+  #  6 temperature
+  #  7	reed gas
+  #  8 voltage
+  #  9 humidity
+  # 10 raw
   # ..31 
-  my %typeStr = (0, "type0", 1,"moisture", 2,"door", 3,"lightHiRange", 4,"lightHiRes", 5,"water", 6,"temp", 7,"reedGas", 8,"voltage",9,"humidity");
-  my %sigStr = (0, "type0", 1,"moisture", 2,"door", 3,"light", 4,"light", 5,"water", 6,"temp", 7,"gas", 8,"voltage",9,"humidity");
+  my %typeStr = (0, "unknown", 1,"moisture", 2,"door", 3,"lightHiRange", 4,"lightHiRes", 5,"water", 6,"temp", 7,"reedGas", 8,"voltage",9,"humidity",10,"raw");
+  my %sigStr = (0, "unknown", 1,"moisture", 2,"door", 3,"light", 4,"light", 5,"water", 6,"temperature", 7,"gas", 8,"voltage",9,"humidity",10,"raw");
   my %batStr = (0,"bad",1,"change",2,"ok",3,"optimal");
   my %trigStr = (0,"auto",1,"manual");
 
   #FHEM Schnittstelle:
-  #    ASTTIIDDDDDD
+  #    ASTTIIDDDDCC
   # AS  - Arduino Sensor
-  # TT  - Bit 0..6 type
-  #	- Bit 7 trigger (0 auto, 1 manual)
-  # II  - Bit 0..5 id
-  #	- Bit 6,7 battery status (00 - bad, 01 - change, 10 - ok, 11 - optimal)
+  # TT  - Bit 0..6 type - Bit 7 trigger (0 auto, 1 manual)
+  # II  - Bit 0..5 id   - Bit 6,7 battery status (00 - bad, 01 - change, 10 - ok, 11 - optimal)
   # DD1 - LowByte
   # DD2 - HighByte
-  
+	# CC  - CRC8 (optional)
+
 	$SensorTyp = "ArduinoSensor";
-    $model = $typeStr{hex(substr($rawData,0,2)) &0x7f}; # Sensortype
+	$model = $typeStr{hex(substr($rawData,0,2)) &0x7f}; # Sensortype
 	$sigType = $sigStr{hex(substr($rawData,0,2)) &0x7f}; # Signaltype
 	$id = hex(substr($rawData,2,2)) &0x3f;
 	$valLow = hex(substr($rawData,4,2));
 	$valHigh = hex(substr($rawData,6,2));
 	$Sigval = (($valHigh<<8) + $valLow);
 	$bat = $batStr{(hex(substr($rawData,2,2))>>6) &3};
+	$batteryState = ((hex(substr($rawData,2,2))>>6) &3) > 0 ? "ok" : "low";
 	$trigger = $trigStr{(hex(substr($rawData,0,2))>>7) &1};
-	Log3 $iohash, 4, "SD_AS Sigval: $Sigval";#
+	Log3 $iohash, 4, "$name: SD_AS Sigval $Sigval";
 
-	if ($model eq "lightHiRange") {
+	if ($model eq "lightHiRange") { # 3
 	  $Sigval = sprintf( "%.1f", $Sigval /1.2); #TBD
 	}
-	elsif ($model eq "lightHiRes") {
+	elsif ($model eq "lightHiRes") { # 4
 	  $Sigval = sprintf( "%.1f", $Sigval /1.2/2);
 	}
-	elsif ($model eq "temp") {
+	elsif ($model eq "temp") { # 6
 	  $Sigval = ($Sigval-0x8000) /10;
-	  Log3 $iohash, 4, "SD_AS: temp out of range" if ($Sigval > 100 || $Sigval < -60);
+	  Log3 $iohash, 3, "$name: SD_AS temp out of range - $Sigval" if ($Sigval > 100 || $Sigval < -60);
 	  return "" if ($Sigval > 100 || $Sigval < -60);
 	  $Sigval = sprintf( "%.1f", $Sigval); #temp is send 10*C
+		$val = "T: $Sigval";
 	}
-	elsif ($model eq "door") {
+	elsif ($model eq "door") { # 2
 	  $Sigval = (($Sigval==255)? 1:0); 
 	}
-	elsif ($model eq "moisture") {
+	elsif ($model eq "moisture") { # 1
 	  $Sigval = sprintf( "%.1f%%", (1024-$Sigval)*100/1024);
 	}
-	elsif ($model eq "humidity") {
+	elsif ($model eq "humidity") { # 9
 	  $Sigval = ($Sigval-0x8000) /10;
-	  Log3 $iohash, 4, "SD_AS: humidity out of range" if ($Sigval > 100 || $Sigval < 0);
+	  Log3 $iohash, 3, "$name: SD_AS humidity out of range - $Sigval" if ($Sigval > 100 || $Sigval < 0);
 	  return "" if ($Sigval > 100 || $Sigval < 0);
-	  
-	  $Sigval = sprintf( "%i %%", $Sigval); #hum is send 10*%
-	  
+		$val = "H: $Sigval";
 	}
-	elsif ($model eq "reedGas") {
+	elsif ($model eq "reedGas") { # 7
 	  $Sigval = sprintf( "%i m3", ($Sigval)); #simple counter, code has to be extended to support restart of the sensor etc.
 	}
-
 	# Bei Voltage Sensoren die Batterieinfo als Zahl ausgeben, damit man sie in einem Diagramm abbilden kann	
-	elsif ($model eq "voltage") {
-	  $bat = (hex(substr($msg,7,2))>>1)&3
-	} else {
-		Log3 $iohash, 4, "SD_AS unknown model: $model";#
-		return undef;
-    }
-    $val = "S: $Sigval B: $bat";
+	elsif ($model eq "voltage") { # 8
+		$batteryVoltage = $Sigval;
+	  # $bat = (hex(substr($msg,7,2))>>1)&3; # ???
+		$bat = hex(substr($rawData,2,2))>>6;
+		$Sigval = round($Sigval / 1000, 2);	# Vcc is send in millivolts
+		$val = "V: $Sigval";
+	}
+	elsif ($model eq "raw") { # 10
+		$bat = hex(substr($rawData,2,2))>>6;
+	  $Sigval = $valHigh * 256 + $valLow;
+		$val = "S: $Sigval";
+	}
+	else { # 0
+		Log3 $iohash, 4, "$name: SD_AS unknown model $model";
+		return;
+  }
+  $val = "S: $Sigval B: $bat" if (!$val);
 
   if ($id ne "") {
     $deviceCode = $model."_".$id;
@@ -212,31 +224,37 @@ SD_AS_Parse($$)
   $def = $modules{AS}{defptr}{$deviceCode} if(!$def);
   
   if(!$def) {
-    Log3 $iohash, 1, "SD_AS: UNDEFINED sensor $SensorTyp detected, code $deviceCode (".$SensorTyp."_".$deviceCode.")";
+    Log3 $iohash, 1, "$name: SD_AS UNDEFINED sensor $SensorTyp detected, code $deviceCode (".$SensorTyp."_".$deviceCode.")";
     return "UNDEFINED ".$SensorTyp."_".$deviceCode." SD_AS $deviceCode";
     #return "UNDEFINED $deviceCode SD_AS $deviceCode";
     
   }
 
+  my $ioname = $name;
   my $hash = $def;
   $name = $hash->{NAME};
   return "" if(IsIgnored($name));
   
-  Log3 $hash, 4, "SD_AS: $name ($msg)";  
+  Log3 $hash, 4, "$ioname: SD_AS $name rawmsg $msg";
   
   $hash->{lastReceive} = time();
 
   if(!$val) {
-    Log3 $hash, 3, "SD_AS: $name: $deviceCode Cannot decode $msg";
+    Log3 $hash, 3, "$ioname: SD_AS $name $deviceCode Cannot decode $msg";
     return "";
   }
   
   $def->{lastMSG} = $msg;
 
-  Log3 $hash, 4, "SD_AS $name: $val";
+  Log3 $hash, 4, "$ioname: SD_AS $name state $val";
+
+	$hash->{timeReceiveDiffLast} = round(gettimeofday() - $hash->{timeReceive} , 1) if defined($hash->{timeReceive});
+	$hash->{timeReceive} = gettimeofday();
 
   readingsBeginUpdate($hash);
   readingsBulkUpdate($hash, "state", $val);
+	readingsBulkUpdate($hash, 'batteryState', $batteryState);
+	readingsBulkUpdate($hash, 'batteryVoltage', $batteryVoltage) if defined $batteryVoltage;
   readingsBulkUpdate($hash, $sigType, $Sigval);
   if ($bat ne "") {
     readingsBulkUpdate($hash, "battery", $bat);
@@ -308,7 +326,6 @@ sub SD_AS_crc($$)
     <li><a href="#do_not_notify">do_not_notify</a></li>
     <li><a href="#eventMap">eventMap</a></li>
     <li><a href="#ignore">ignore</a></li>
-    <li><a href="#model">model</a> (LogiLink Env)</li>
     <li><a href="#showtime">showtime</a></li>
     <li><a href="#readingFnAttributes">readingFnAttributes</a></li>
   </ul>
@@ -347,7 +364,6 @@ sub SD_AS_crc($$)
     <li><a href="#do_not_notify">do_not_notify</a></li>
     <li><a href="#eventMap">eventMap</a></li>
     <li><a href="#ignore">ignore</a></li>
-    <li><a href="#model">model</a> (LogiLink Env)</li>
     <li><a href="#showtime">showtime</a></li>
     <li><a href="#readingFnAttributes">readingFnAttributes</a></li>
   </ul>
--- FHEM/lib/SD_ProtocolData.pm
+++ FHEM/lib/SD_ProtocolData.pm
@@ -87,7 +87,7 @@ package lib::SD_ProtocolData;
   use strict;
   use warnings;
 
-  our $VERSION = '1.22';
+  our $VERSION = '1.23';
 
   our %protocols = (
     "0" =>  ## various weather sensors (500 | 9100)
@@ -238,6 +238,9 @@ package lib::SD_ProtocolData;
         length_max       => '40',          # 24 | userMSG 32 ?
       },
     "2"  =>   ## Self build arduino sensor
+             # ArduinoSensor_temp_2      T: 21.0  MS;P1=-463;P2=468;P3=-1043;P5=-9981;D=252121212121232321232321212121232123232123212123212321212121212121232321212123212324;CP=2;SP=5;R=16;O;m2;
+             # ArduinoSensor_humidity_2  H: 61.9  MS;P0=-491;P2=523;P4=-991;P7=-9972;D=272020202024202024242420202020242020242420242024242420202020202420202424242420242426;CP=2;SP=7;m2;
+             # ArduinoSensor_voltage_2   V: 3.65  MS;P0=-10406;P1=513;P2=-437;P4=-1013;D=10121212121412121214141212121214121212141414141214121212121414141212141214141214121;CP=1;SP=0;
       {
         name             => 'Arduino',
         comment          => 'self build arduino sensor (developModule. SD_AS module only in github)',
@@ -251,10 +254,9 @@ package lib::SD_ProtocolData;
         format           => 'twostate',
         preamble         => 'P2#',
         clientmodule     => 'SD_AS',
-        modulematch      => '^P2#.{7,8}',
-        length_min       => '32',
-        length_max       => '34',        # Don't know maximal lenth of a valid message
-        paddingbits      => '8',         # pad up to 8 bits, default is 4
+        modulematch      => '^P2#.{8,10}',
+        length_min       => '32', # without CRC
+        length_max       => '40', # with CRC
       },
     "3"  =>  ## itv1 - remote with IC PT2262 example: ELRO | REWE | Intertek Modell 1946518 | WOFI Lamp // PIR JCHENG with Wireless Coding EV1527
              ## (real CP=300 | repeatpause=9300)
