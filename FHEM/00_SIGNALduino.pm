0597cc558838f909264a25572e887b999f365556
sidey79 <7968127+sidey79@users.noreply.github.com>
2021-07-08 23:36:49 +0200

    00_SIGNALduino.pm
     00_SIGNALduino
    
    SIGNALduino_PatternExists überarbeitet um hohe CPU Last und Speicherverbrauch zu umgehen
    Fixes High memory and CPU usage  in SIGNALduino_PatternExists  #986

--- FHEM/00_SIGNALduino.pm
+++ FHEM/00_SIGNALduino.pm
@@ -1,6 +1,6 @@
-# $Id: 00_SIGNALduino.pm v3.5.1 2020-07-02 21:20:33Z Sidey $
+# $Id: 00_SIGNALduino.pm v3.5.2 2020-07-02 21:20:33Z Sidey $
 #
-# v3.5.1 - https://github.com/RFD-FHEM/RFFHEM/tree/master
+# v3.5.2 - https://github.com/RFD-FHEM/RFFHEM/tree/master
 # The module is inspired by the FHEMduino project and modified in serval ways for processing the incoming messages
 # see http://www.fhemwiki.de/wiki/SIGNALDuino
 # It was modified also to provide support for raw message handling which can be send from the SIGNALduino
@@ -39,7 +39,7 @@ use List::Util qw(first);
 
 
 use constant {
-  SDUINO_VERSION                  => '3.5.2+20210616',
+  SDUINO_VERSION                  => '3.5.2+20210708',
   SDUINO_INIT_WAIT_XQ             => 1.5,     # wait disable device
   SDUINO_INIT_WAIT                => 2,
   SDUINO_INIT_MAXRETRY            => 3,
@@ -1264,14 +1264,10 @@ sub SIGNALduino_CheckSendRawResponse {
     my $name=$hash->{NAME};
     # zu testen der sendeQueue, kann wenn es funktioniert auf verbose 5
     $hash->{logMethod}->($name, 4, "$name: CheckSendrawResponse, sendraw answer: $msg");
+    #RemoveInternalTimer("HandleWriteQueue:$name");
     delete($hash->{ucCmd});
-    if ($msg =~ /D=[A-Za-z0-9]+;/ )
-    {
-      RemoveInternalTimer("HandleWriteQueue:$name");
-      SIGNALduino_HandleWriteQueue("x:$name"); # Todo #823 on github
-    } else {
-      InternalTimer(gettimeofday() , \&SIGNALduino_HandleWriteQueue, "HandleWriteQueue:$name") if (scalar @{$hash->{QUEUE}} > 0 && InternalVal($name,'sendworking',0) == 0);
-    }
+    #SIGNALduino_HandleWriteQueue("x:$name"); # Todo #823 on github
+    InternalTimer(gettimeofday() + 0.1, \&SIGNALduino_HandleWriteQueue, "HandleWriteQueue:$name") if (scalar @{$hash->{QUEUE}} > 0 && InternalVal($name,'sendworking',0) == 0);
   }
   return (undef);
 }
@@ -1624,7 +1620,7 @@ sub SIGNALduino_AddSendQueue {
   #SIGNALduino_Log3 $hash , 5, Dumper($hash->{QUEUE});
 
   $hash->{logMethod}->($hash, 5,"$name: AddSendQueue, " . $hash->{NAME} . ": $msg (" . @{$hash->{QUEUE}} . ')');
-  InternalTimer(gettimeofday(), \&SIGNALduino_HandleWriteQueue, "HandleWriteQueue:$name") if (scalar @{$hash->{QUEUE}} == 1 && InternalVal($name,'sendworking',0) == 0);
+  InternalTimer(gettimeofday() + 0.1, \&SIGNALduino_HandleWriteQueue, "HandleWriteQueue:$name") if (scalar @{$hash->{QUEUE}} == 1 && InternalVal($name,'sendworking',0) == 0);
 }
 
 ############################# package main, test exists
@@ -1814,11 +1810,9 @@ sub SIGNALduino_Read {
             $hash->{logMethod}->($name, 5, "$name: Read, try asyncOutput of message $returnMessage");
             my $ao = undef;
             $ao = asyncOutput( $hash->{ucCmd}->{asyncOut}, $hash->{ucCmd}->{cmd}.': ' . $returnMessage ) if (defined($returnMessage));
-            $hash->{logMethod}->($name, 5, "$name: Read, asyncOutput failed $ao") if (defined($ao));
+          $hash->{logMethod}->($name, 5, "$name: Read, asyncOutput failed $ao") if (defined($ao));
           }
-          if ( exists $hash->{ucCmd} && defined $hash->{ucCmd}->{cmd} &&  $hash->{ucCmd}->{cmd} ne "sendraw" ) {
-            delete $hash->{ucCmd} ;
-          }
+          delete($hash->{ucCmd});
         }
 
         if (exists($hash->{keepalive})) {
@@ -1974,15 +1968,16 @@ sub SIGNALduino_PatternExists {
   #Debug 'plist: '.Dumper($patternList) if($debug);
   #Debug 'searchlist: '.Dumper($search) if($debug);
 
-  my $valid=1;
-  my @pstr;
   my $debug = AttrVal($hash->{NAME},'debug',0);
   my $i=0;
-  my $maxcol=0;
+  my @indexer;
+  my @sumlist;
+  my %plist=();
 
-  foreach my $searchpattern (@{$search})    # z.B. [1, -4]
+  for my $searchpattern (@{$search})    # z.B. [1, -4]
   {
-    #my $patt_id;
+    next if (exists $plist{$searchpattern});
+
     # Calculate tolernace for search
     #my $tol=abs(abs($searchpattern)>=2 ?$searchpattern*0.3:$searchpattern*1.5);
     my $tol=abs(abs($searchpattern)>3 ? abs($searchpattern)>16 ? $searchpattern*0.18 : $searchpattern*0.3 : 1);  #tol is minimum 1 or higer, depending on our searched pulselengh
@@ -1998,53 +1993,66 @@ sub SIGNALduino_PatternExists {
       # Extract fist pattern, which is nearst to our searched value
       my @closestidx = (sort {$pattern_gap{$a} <=> $pattern_gap{$b}} keys %pattern_gap);
 
-      my $idxstr='';
-      my $r=0;
-
-      while (my ($item) = splice(@closestidx, 0, 1))
-      {
-        $pstr[$i][$r]=$item;
-        $r++;
-        Debug "closest pattern has index: $item" if($debug);
-      }
-      $valid=1;
+      $plist{$searchpattern} = 1;
+      push @indexer, $searchpattern; 
+      push @sumlist, [@closestidx];  
     } else {
       # search is not found, return -1
       return -1;
-      last;
     }
     $i++;
-    #return ($valid ? $pstr : -1);  # return $pstr if $valid or -1
-
-    #foreach $patt_id (keys %$patternList) {
-      #Debug "$patt_id. chk ->intol $patternList->{$patt_id} $searchpattern $tol";
-      #$valid =  SIGNALduino_inTol($patternList->{$patt_id}, $searchpattern, $tol);
-      #if ( $valid) #one pulse found in tolerance, search next one
-      #{
-      # $pstr="$pstr$patt_id";
-      # # provide this index for further lookup table -> {$patt_id =  $searchpattern}
-      # Debug 'pulse found';
-      # last ; ## Exit foreach loop if searched pattern matches pattern in list
-      #}
-    #}
-    #last if (!$valid);  ## Exit loop if a complete iteration has not found anything
   }
-  my @results = ('');
 
-  foreach my $subarray (@pstr)
+  sub cartesian_product {
+    use List::Util qw(reduce);
+    reduce {
+      [ map {
+        my $item = $_;
+        map [ @$_, $item ], @$a
+      } @$b ]
+    } [[]], @_
+  }
+  my @res = cartesian_product @sumlist;
+  Debug qq[sumlists is: ].Dumper @sumlist if($debug);
+  Debug qq[res is: ].Dumper $res[0] if($debug);
+  Debug qq[indexer is: ].Dumper \@indexer if($debug);
+
+  OUTERLOOP:
+  for my $i (0..$#{$res[0]})
   {
-    @results = map {my $res = $_; map $res.$_, @$subarray } @results;
+
+    ## Check if we have same patternindex for different values and skip this invalid ones
+    my %count;  
+    for (@{$res[0][$i]}) 
+    { 
+      $count{$_}++; 
+      next OUTERLOOP if ($count{$_} > 1)
+    };
+    
+    # Create a mapping table to exchange the values later on
+    for (my $x=0;$x <= $#indexer;$x++)
+    {
+      $plist{$indexer[$x]}  = $res[0][$i][$x]; 
+    }
+    Debug qq[plist is for this check ].Dumper(\%plist) if($debug);
+
+    # Create our searchstring with our mapping table
+    my @patternVariant= @{$search};
+    for my $v (@patternVariant)
+    {
+      #Debug qq[value before is: $v ] if($debug);
+      $v = $plist{$v};
+      #Debug qq[after: $v ] if($debug);
+
+    }
+    Debug qq[patternVariant is ].Dumper(\@patternVariant) if($debug);
+    my $search_pattern = join '', @patternVariant;
+
+    (index ($$data, $search_pattern) > -1) ? return $search_pattern : next;
+    
   }
+  return -1;  
 
-  foreach my $search (@results)
-  {
-    Debug "looking for substr $search" if($debug);
-    return $search if (index( ${$data}, $search) >= 0);
-  }
-
-  return -1;
-
-  #return ($valid ? @results : -1);  # return @pstr if $valid or -1
 }
 
 ############################# package main
@@ -3266,7 +3274,6 @@ function SD_plistWindow(txt)
 sub SIGNALduino_FW_saveWhitelist {
   my $name = shift;
   my $wl_attr = shift;
-  my $hash = $defs{$name};
 
   if (!IsDevice($name)) {
     Log3 undef, 3, "$name: FW_saveWhitelist, is not a valid definition, operation aborted.";
@@ -3283,7 +3290,7 @@ sub SIGNALduino_FW_saveWhitelist {
   else {
     $wl_attr =~ s/,$//;   # Komma am Ende entfernen
   }
-  CommandAttr($hash,"$name whitelist_IDs $wl_attr");
+  $attr{$name}{whitelist_IDs} = $wl_attr;
   Log3 $name, 3, "$name: FW_saveWhitelist, $wl_attr";
   SIGNALduino_IdList("x:$name", $wl_attr);
 }
