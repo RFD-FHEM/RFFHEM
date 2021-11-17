123162fb41877f465dd51c2a5331b6cbd0eaa06d
GitHub Action <action@github.com>
2021-11-17 22:20:35 +0000

    Update Versiondate

--- FHEM/10_FS10.pm
+++ FHEM/10_FS10.pm
@@ -1,5 +1,5 @@
 ##############################################
-# $Id: 10_FS10.pm 11 2021-11-17 21:48:53Z sidey79 $
+# $Id: 10_FS10.pm 11 2021-11-17 22:19:37Z sidey79 $
 #
 # FS10 basierend auf dem FS20 Modul angepasst fuer SIGNALduino, elektron-bbs
 #
