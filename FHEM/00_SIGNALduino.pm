ba7672c74ec3a4968f2867787e6d8f36900e3957
GitHub Action <action@github.com>
2022-01-08 20:57:35 +0000

    Update Versiondate

--- FHEM/00_SIGNALduino.pm
+++ FHEM/00_SIGNALduino.pm
@@ -1,4 +1,4 @@
-# $Id: 00_SIGNALduino.pm v3.5.2 2022-01-08 20:38:26Z sidey79 $
+# $Id: 00_SIGNALduino.pm v3.5.2 2022-01-08 20:57:04Z sidey79 $
 # v3.5.2 - https://github.com/RFD-FHEM/RFFHEM/tree/master
 # The module is inspired by the FHEMduino project and modified in serval ways for processing the incoming messages
 # see http://www.fhemwiki.de/wiki/SIGNALDuino
