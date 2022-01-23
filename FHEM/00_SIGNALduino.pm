c01b583db1e1a5b91d6f87b3904db8a1c9e9fcbd
sidey79 <7968127+sidey79@users.noreply.github.com>
2022-01-23 11:36:52 +0100

    Master 3.5.3 (#1068)
    Release 3.5.3 
        00_SIGNALduino.pm:
              feature: Handling of missing modules changed
        
        SD_Protocols.pm
              feature: Graceful load JSON and Digest:CRC Modules (#1066)
    
        14_SD_UT.pm
              bugfix: fix buttons P90 (#1060)

--- FHEM/00_SIGNALduino.pm
+++ FHEM/00_SIGNALduino.pm
@@ -1,4 +1,4 @@
-# $Id: 00_SIGNALduino.pm v3.5.3 2022-01-22 16:38:42Z sidey79 $
+# $Id: 00_SIGNALduino.pm v3.5.3 2022-01-22 22:08:08Z sidey79 $
 # v3.5.3 - https://github.com/RFD-FHEM/RFFHEM/tree/master
 # The module is inspired by the FHEMduino project and modified in serval ways for processing the incoming messages
 # see http://www.fhemwiki.de/wiki/SIGNALDuino
@@ -14,7 +14,7 @@
 package main;
 use strict;
 use warnings;
-#use version 0.77; our $VERSION = version->declare('v3.5.3+20220116');
+#use version 0.77; our $VERSION = version->declare('v3.5.3');
 
 my $missingModulSIGNALduino = ' ';
 
@@ -38,7 +38,7 @@ use List::Util qw(first);
 
 
 use constant {
-  SDUINO_VERSION                  => '3.5.2+20220122',  # Datum wird automatisch bei jedem pull request aktualisiert
+  SDUINO_VERSION                  => '3.5.3',  # Datum wird automatisch bei jedem pull request aktualisiert
   SDUINO_INIT_WAIT_XQ             => 1.5,     # wait disable device
   SDUINO_INIT_WAIT                => 2,
   SDUINO_INIT_MAXRETRY            => 3,
--- FHEM/lib/SD_ProtocolData.pm
+++ FHEM/lib/SD_ProtocolData.pm
@@ -1,5 +1,5 @@
 ###########################################################################################################################################
-# $Id: SD_ProtocolData.pm 3.4.4 2022-01-15 12:25:06Z elektron-bbs $
+# $Id: SD_ProtocolData.pm 3.5.3 2022-01-22 22:08:08Z sidey79 $
 # The file is part of the SIGNALduino project.
 # All protocol definitions are contained in this file.
 #
