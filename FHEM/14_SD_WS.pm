8ae8927ad807956a3b03073a4c40b76d2e2a1ba1
sidey79 <7968127+sidey79@users.noreply.github.com>
2022-03-20 10:54:58 +0100

    Update 14_SD_WS.pm
    Update Version in File

--- FHEM/14_SD_WS.pm
+++ FHEM/14_SD_WS.pm
@@ -1,4 +1,4 @@
-# $Id: 14_SD_WS.pm 21666 2022-01-30 10:19:30Z elektron-bbs $
+# $Id: 14_SD_WS.pm v3.5.4 2022-01-30 10:19:30Z elektron-bbs $
 #
 # The purpose of this module is to support serval
 # weather sensors which use various protocol
@@ -46,7 +46,7 @@
 
 package main;
 
-#use version 0.77; our $VERSION = version->declare('v3.4.3');
+#use version 0.77; our $VERSION = version->declare('v3.5.4');
 
 use strict;
 use warnings;
