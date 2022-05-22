7e85e242c8ebf177ba7d510f3d852d6daad962c3
GitHub Action <action@github.com>
2022-05-22 21:11:03 +0000

    Update Versiondate

--- FHEM/lib/SD_ProtocolData.pm
+++ FHEM/lib/SD_ProtocolData.pm
@@ -1,5 +1,5 @@
 ###########################################################################################################################################
-# $Id: SD_ProtocolData.pm 3.5.4 2022-05-22 20:39:44Z sidey79 $
+# $Id: SD_ProtocolData.pm 3.5.4 2022-05-22 21:10:26Z sidey79 $
 # The file is part of the SIGNALduino project.
 # All protocol definitions are contained in this file.
 #
