0abe448e9f280c9be22e8ff1d179ed42f421b762
GitHub Action <action@github.com>
2022-05-30 20:11:33 +0000

    Update Versiondate

--- FHEM/lib/SD_Protocols.pm
+++ FHEM/lib/SD_Protocols.pm
@@ -1,5 +1,5 @@
 ################################################################################
-# $Id: SD_ProtocolData.pm 3.5.x 2022-05-30 19:34:38Z sidey79 $
+# $Id: SD_ProtocolData.pm 3.5.x 2022-05-30 20:10:51Z sidey79 $
 #
 # The file is part of the SIGNALduino project
 # v3.5.x - https://github.com/RFD-FHEM/RFFHEM
