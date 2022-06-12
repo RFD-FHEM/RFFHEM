54d9c634cf05e49ff26714c4ff6d92a4aa3ef5ed
GitHub Action <action@github.com>
2022-06-12 21:56:19 +0000

    Update Versiondate

--- FHEM/14_Hideki.pm
+++ FHEM/14_Hideki.pm
@@ -1,5 +1,5 @@
 ##############################################
-# $Id: 14_Hideki.pm 21666 2022-06-09 18:31:42Z HomeAutoUser $
+# $Id: 14_Hideki.pm 21666 2022-06-12 21:55:30Z HomeAutoUser $
 # The file is taken from the SIGNALduino project
 # see http://www.fhemwiki.de/wiki/SIGNALduino
 # and was modified by a few additions
