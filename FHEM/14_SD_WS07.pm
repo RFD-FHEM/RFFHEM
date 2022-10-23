101d9f8c3f7fd2628cdbef8c7e05eac06f9c1baa
GitHub Action <action@github.com>
2022-10-23 12:40:42 +0000

    Update Versiondate

--- FHEM/14_SD_WS07.pm
+++ FHEM/14_SD_WS07.pm
@@ -1,5 +1,5 @@
 ##############################################
-# $Id: 14_SD_WS07.pm 21666 2021-12-29 16:12:11Z elektron-bbs $
+# $Id: 14_SD_WS07.pm 21666 2022-10-23 12:40:10Z sidey79 $
 # 
 # The purpose of this module is to support serval eurochron
 # weather sensors like eas8007 which use the same protocol
