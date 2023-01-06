7174414ddfc215cba8523127e8588eca93f95320
sidey79 <7968127+sidey79@users.noreply.github.com>
2023-01-06 13:19:41 +0100

    00_SIGNALduino.pm
    bump version to 3.5.5

--- FHEM/00_SIGNALduino.pm
+++ FHEM/00_SIGNALduino.pm
@@ -1,4 +1,4 @@
-# $Id: 00_SIGNALduino.pm 26977 2023-01-06 12:07:45Z sidey79 $
+# $Id: 00_SIGNALduino.pm 3.5.5 2023-01-06 12:07:45Z sidey79 $
 # v3.5.4 - https://github.com/RFD-FHEM/RFFHEM/tree/master
 # The module is inspired by the FHEMduino project and modified in serval ways for processing the incoming messages
 # see http://www.fhemwiki.de/wiki/SIGNALDuino
@@ -15,7 +15,7 @@ package main;
 use strict;
 use warnings;
 use Storable qw(dclone); 
-#use version 0.77; our $VERSION = version->declare('v3.5.4');
+#use version 0.77; our $VERSION = version->declare('v3.5.5');
 
 my $missingModulSIGNALduino = ' ';
 
@@ -41,7 +41,7 @@ use List::Util qw(first);
 
 
 use constant {
-  SDUINO_VERSION                  => '3.5.4',  # Datum wird automatisch bei jedem pull request aktualisiert
+  SDUINO_VERSION                  => '3.5.5+20230106',  # Datum wird automatisch bei jedem pull request aktualisiert
   SDUINO_INIT_WAIT_XQ             => 1.5,     # wait disable device
   SDUINO_INIT_WAIT                => 2,
   SDUINO_INIT_MAXRETRY            => 3,
@@ -5599,7 +5599,7 @@ USB-connected devices (SIGNALduino):<br>
       "web": "https://wiki.fhem.de/wiki/SIGNALduino"
     }
   },
-  "version": "v3.5.4"
+  "version": "v3.5.5"
 }
 =end :application/json;q=META.json
 =cut
