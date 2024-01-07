c5e26fe53a9eee3372d625053ca9b9e1082451bf
GitHub Action <action@github.com>
2024-01-07 20:20:52 +0000

    Update Versiondate

--- FHEM/14_SD_WS.pm
+++ FHEM/14_SD_WS.pm
@@ -1,4 +1,4 @@
-# $Id: 14_SD_WS.pm 26982 2024-01-07 15:31:22Z elektron-bbs $
+# $Id: 14_SD_WS.pm 26982 2024-01-07 20:20:20Z elektron-bbs $
 #
 # The purpose of this module is to support serval
 # weather sensors which use various protocol
