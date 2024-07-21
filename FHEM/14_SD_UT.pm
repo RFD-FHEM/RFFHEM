7bc8f7db37a5db93f678a6ec855a0b4c1203457c
GitHub Action <action@github.com>
2024-07-21 10:14:27 +0000

    Update Versiondate

--- FHEM/14_SD_UT.pm
+++ FHEM/14_SD_UT.pm
@@ -1,5 +1,5 @@
 #########################################################################################
-# $Id: 14_SD_UT.pm 0 2024-07-20 11:36:40Z elektron-bbs $
+# $Id: 14_SD_UT.pm 0 2024-07-21 10:13:54Z elektron-bbs $
 #
 # The file is part of the SIGNALduino project.
 # The purpose of this module is universal support for devices.
